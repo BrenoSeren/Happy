9eff2a33e2b2bf7a9efaee1c5b64929ac5ccd5e7
Rafael-Amorim7 <rafaamorim1504@gmail.com>
2021-10-17 14:10:20 -0300

    models added

--- app/models.py
+++ app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+class Estado(models.Model):
+    name = models.CharField(max_length=45)
+
+class Cidade (models.Model):
+    Estado_id_estado = models.ForeignKey(
+        Estado,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=45) 
+
+class Institution(models.Model):
+    Estado_id_estado = models.ForeignKey(
+        Estado,
+        on_delete=models.CASCADE
+    )
+    Cidade_id_cidade = models.ForeignKey(
+        Cidade,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(max_length=100)
+    email = models.CharField(max_length=50)
+    celular = models.CharField(max_length=11)
+    atvidade = models.CharField(max_length=45)
+    descricao = models.TextField()
+    cep = models.CharField(max_length=9)
+    bairro = models.CharField(max_length=100)
+    endereco = models.CharField(max_length=100)
+    situacao = models.CharField(max_length=1)
+    created_at = models.TimeField(auto_now=True)
+    updated_at = models.TimeField()
