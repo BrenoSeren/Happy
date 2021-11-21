460a25293c8f01f1fc10afd274d66f9ecbb99ecd
Rafael-Amorim7 <rafaamorim1504@gmail.com>
2021-11-21 10:33:19 -0300

    handle back end empty string finish

--- map/views.py
+++ map/views.py
@@ -4,16 +4,16 @@ import geocoder
 
 # Create your views here.
 def map(req):
-    adress = req.POST.get('address')
-    if (not adress):
-        adress = 'Marília'
+    adress = req.POST.get('adress')
+    if adress == None:
+        adress = 'São Paulo'
     location = geocoder.osm(adress)
     lat = location.lat
     lng = location.lng
     country = location.country
     # Create Map Object
     m = folium.Map(location=[-15.7801,-47.9292], zoom_start=4)
-    folium.Marker([lat,lng], tooltip='Click for more information', popup=country).add_to(m)
+    folium.Marker([lat,lng], tooltip='Clique para mais informações', popup=country).add_to(m)
     m = m._repr_html_()
     context={
         'm':m,
